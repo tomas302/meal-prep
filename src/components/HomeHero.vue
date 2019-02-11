34aaf4048b0b8c0aa3b9a802789926636d2bc08b
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-11 19:46:10 +0100

    Added AppNavigation component Added Vuetify to the App file Created HomeHero component

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fluid fill-height class="home-hero">
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeHero'
+};
+</script>
+
+<style scoped>
+.home-hero {
+    background: url('http://source.unsplash.com/0BhSKStVtdM');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
