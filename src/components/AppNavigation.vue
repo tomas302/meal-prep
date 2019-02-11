7df56a8bdde73af06ed03f6ca64726bbceac1152
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-11 23:46:24 +0100

    Added Authentication with Firebase Added addRecipe and getUserRecipes (Vuex & Firebase) Added MealRecipes component Added Firebase configuration Added About, Join, Menu and Signin view

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -5,9 +5,7 @@
                 <template v-for="(item, index) in items">
                     <v-list-tile :key="index">
                         <v-list-tile-content>
-                            <router-link :to="item.route">
-                                {{item.title}}
-                            </router-link>
+                            {{item.title}}
                         </v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${index}`"></v-divider>
@@ -22,8 +20,15 @@
             </router-link>
             <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn flat class="hidden-sm-and-down" to="/sing-in">SING IN</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join">JOIN</v-btn>
+            <div v-if="!isAuthenticated" class="hidden-sm-and-down">
+                <v-btn flat to="/sign-in">SIGN IN</v-btn>
+                <v-btn color="brown lighten-3" to="/join">JOIN</v-btn>
+            </div>
+            <div v-else>
+                <v-btn flat to="/about">PROFILE</v-btn>
+                <v-btn outline color="white" @click="logout">Logout</v-btn>
+            </div>
+
         </v-toolbar>
     </span>
 </template>
@@ -35,12 +40,18 @@ export default {
         return {
             appTitle: 'Meal Prep',
             drawer: false,
-            items: [
-                { title: 'Menu', route: '/menu' },
-                { title: 'Sign In', route: '/sign-in' },
-                { title: 'Join', route: '/join' }
-            ]
+            items: [{ title: 'Menu' }, { title: 'Sign In' }, { title: 'Join' }]
         };
+    },
+    computed: {
+        isAuthenticated() {
+            return this.$store.getters.isAuthenticated;
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('userSignOut');
+        }
     }
 };
 </script>
---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDxLAVzOWC4d6fPqPh0l3NpM-JFgWhUYUI",
+    authDomain: "meal-prep-b95aa.firebaseapp.com",
+    databaseURL: "https://meal-prep-b95aa.firebaseio.com",
+    projectId: "meal-prep-b95aa",
+    storageBucket: "",
+    messagingSenderId: "208416473720"
+};
+firebase.initializeApp(config);
